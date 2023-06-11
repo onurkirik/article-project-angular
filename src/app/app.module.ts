924454c7edccb2e05f4b0cca687f9bcb90c8d660
Onur Kırık <kirikonurr@gmail.com>
2023-06-11 19:32:49 +0300

    last changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,32 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { CreateArticleComponent } from './create-article/create-article.component';
+import { MatInputModule } from '@angular/material/input';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularEditorModule } from '@kolkov/angular-editor';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    CreateArticleComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule,
+    HttpClientModule,
+    AngularEditorModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
